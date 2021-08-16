7d438393fe8c1ea44b7dc4f7c4be7336de398941
Tim Robinson <timrob584@gmail.com>
2021-08-16 14:01:32 -0400

    added functions and notes in js file. removed the onClick in html

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add each letter to array
+var lowerChar = ["a", "b", ]
+// Add array for uppercase, numeric, & special
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,15 +28,14 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // I have gotten it to have an alert window pop up but there is no text in it?
-function prompt() {
-  var criteria = prompt("Please designate criteria");
-  alert (criteria);
-}
+// function prompt() {
+//   var criteria = prompt("Please designate criteria");
+// }
 
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
 
-function prompt() {
+function prompt2() {
   var included = prompt("Include  ")
 }
 
@@ -39,17 +46,10 @@ function Length() {
   var length = prompt("Desired Character Length", length)
 }
 
-generateBtn.addEventListener("click", okay, cancel);
+// generateBtn.addEventListener("click", okay, cancel);
 
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-function prompt() {
-var lowercase = prompt("Include Lowercase?")
-var uppercase = prompt("Include Uppercase?")
-var numeric = prompt("Include Numbers?")
-var special = prompt("Include Special Characters?")
-}
 
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
@@ -59,34 +59,91 @@ var special = prompt("Include Special Characters?")
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 
-var password = generatePassword(
-  if (lowercase = yes) {
-    include lowercase
-  } else {
-    do not include lowercase
-  }
+function generatePassword() {
 
-  if (uppercase = yes) {
-    include uppercase
-  } else {
-    do not include uppercase
-  }
+  var passPrompt = userPrompt();
 
-  if (number = yes) {
-    include number
-  } else {
-    do not include number
-  }
 
-  if (special = yes) {
-    include special
-  } else {
-    do not include special
-  }
-);
+  var result = [];
+  var possibleChar = [];
+  var guaranteedChar = [];
 
-resultDOM.innerText = password;
+  // concat()
+  // push()
+
+  //use getRandom in here
+
+  // if (lowercase = yes) {
+  //   include lowercase
+  // } else {
+  //   do not include lowercase
+  // }
+
+  // if (uppercase = yes) {
+  //   include uppercase
+  // } else {
+  //   do not include uppercase
+  // }
+
+  // if (number = yes) {
+  //   include number
+  // } else {
+  //   do not include number
+  // }
+
+  // if (special = yes) {
+  //   include special
+  // } else {
+  //   do not include special
+  // }
+};
+
+function getRandom() {
+  //research Math.floor(Math.random)
+}
+
+// resultDOM.innerText = password;
 
 
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
+
+
+
+// COLLECTIVE USER INPUT FUNCTION
+
+function userPrompts() {
+  var length = prompt("Desired Character Length")
+
+if (length < 8 || length > 128) {
+alert ("Please choose a character length between 8-128");
+return;
+}
+
+var lowercase = confirm("Include Lowercase?")
+var uppercase = confirm("Include Uppercase?")
+var numeric = confirm("Include Numbers?")
+var special = confirm("Include Special Characters?");
+
+console.log(lowercase, uppercase, numeric, special);
+
+if (lowercase === false && uppercase === false && numeric === false && special === false) {
+  alert ("Please designate a character type for the password");
+  return;
+}
+
+var userInput = {
+  length:length,
+  lowercase:lowercase,
+  uppercase:uppercase,
+  numeric:numeric,
+  special:special
+}
+
+console.log (userInput);
+
+return userInput;
+
+}
+
+
---
+++ notes.md
@@ -0,0 +1,9 @@
+# Notes
+
+Pop up alerts
+1. prompt - Ask a question that the user will fill out an input
+2. alert - Just sends a message
+3. confirm - boolean true/false question
+
+Use console.log to test my work. Remove it before submitting as it is for test only.
+
